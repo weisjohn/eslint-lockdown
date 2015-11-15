33af2a99a95781a1489c96b3dee917bd7b59837f
John Weis <weis.john@gmail.com>
2015-11-14 23:26:02 -0600

    initial api written

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 
 var CLIEngine = require('eslint').CLIEngine;
 var _ = require('lodash');
+var path = require('path');
+var fs = require('fs');
 
-module.exports = function() {
+module.exports = function(cwd, cb) {
     var cli = new CLIEngine({});
 
     var report = cli.executeOnFiles(['.']);
 
-    var res = _(CLIEngine.getErrorResults(report.results))
+    var rules = _(CLIEngine.getErrorResults(report.results))
         .pluck('messages')
         .flatten()
         .pluck('ruleId')
         .uniq()
-        .value();
+        .reduce(function(prev, current) {
+            prev[current] = [ 1 ];
+            return prev;
+        }, {});
 
-    console.log(res);
+    var p = path.join(cwd, '.eslintrc');
+
+    fs.access(p, fs.R_OK, function(err) {
+        if (err) return cb(err);
+        fs.readFile(p, function(err, file) {
+            if (err) return cb(err);
+            var config = JSON.parse(file);
+
+            config.rules = _.assign(config.rules || {}, rules);
+
+            cb(null, config);
+        });
+    });
 
 };
