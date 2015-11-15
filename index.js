fc32c396e52777cd2a0818ec4e55a86f13656584
John Weis <weis.john@gmail.com>
2015-11-15 14:17:40 -0600

    moving file writing out of lib

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
 var CLIEngine = require('eslint').CLIEngine;
 var _ = require('lodash');
-var path = require('path');
-var fs = require('fs');
 
-module.exports = function(cwd, cb) {
+module.exports = function(cwd, config, cb) {
+
     var cli = new CLIEngine({});
-
     var report = cli.executeOnFiles(['.']);
 
     var rules = _(CLIEngine.getErrorResults(report.results))
@@ -19,18 +17,7 @@ module.exports = function(cwd, cb) {
             return prev;
         }, {});
 
-    var p = path.join(cwd, '.eslintrc');
+    config.rules = _.assign(config.rules || {}, rules);
 
-    fs.access(p, fs.R_OK, function(err) {
-        if (err) return cb(err);
-        fs.readFile(p, function(err, file) {
-            if (err) return cb(err);
-            var config = JSON.parse(file);
-
-            config.rules = _.assign(config.rules || {}, rules);
-
-            cb(null, config);
-        });
-    });
-
+    cb(null, config);
 };
