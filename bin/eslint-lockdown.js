5a3f078e99ce048d9515a4d023eb68270611ecb9
John Weis <weis.john@gmail.com>
2015-11-15 14:18:47 -0600

    doing the file write in node to keep it clean

--- bin/eslint-lockdown.js
+++ bin/eslint-lockdown.js
@@ -3,10 +3,54 @@
 var program = require('commander');
 var pkg = require('../package');
 var lockdown = require('../');
+var path = require('path');
+var fs = require('fs');
+var cwd = process.cwd();
+var file = path.join(cwd, '.eslintrc');
 
-program.version(pkg.version).parse(process.argv);
+program
+    .version(pkg.version)
+    .option('-d', '--debug', 'Don\'t overwrite the `.eslintrc` file')
+    .parse(process.argv);
 
-lockdown(process.cwd(), function(err, res) {
-    if (err) return console.error(err);
-    console.log(JSON.stringify(res, null, 4));
+function run(config, cb) {
+    lockdown(cwd, config, function(err, res) {
+        if (err) return cb(err);
+        return cb(null, config);
+    });
+}
+
+function read(cb) {
+    fs.access(file, fs.R_OK, function(err) {
+        if (err) return cb(err);
+        fs.readFile(file, function(err, file) {
+            if (err) return cb(err);
+            var config = {};
+            try {
+                config = JSON.parse(file);
+            } catch (e) {
+                return cb(e);
+            }
+            return cb(null, config);
+        });
+    });
+}
+
+function write(config, cb) {
+    console.log('file', file);
+    fs.writeFile(file, JSON.stringify(config, null, 4), cb);
+}
+
+// all together now
+read(function(err, config) {
+    if (err || !config) config = {};
+    run(config, function(err, config) {
+        if (err) return console.error(err);
+        console.log(program);
+
+        if (program.debug) return console.log(config);
+        write(config, function(err) {
+            if (err) return console.error(err);
+        });
+    });
 });
